7c63dfcd8e0ee87415d7e520253a2549d324afb0
Wallace <wallace_wolv@hotmail.com>
2021-04-20 22:41:07 -0300

    Final LevelButtonWidget lest function  Ontap

--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -4,18 +4,46 @@ import 'package:google_fonts/google_fonts.dart';
 
 class LevelButtonWidget extends StatelessWidget {
   final String label;
-  const LevelButtonWidget({
+  LevelButtonWidget({
     Key? key,
     required this.label,
-  }) : super(key: key);
+  })   : assert(["Fácil", "Médio", "Difícil", "Perito"].contains(label)),
+        super(key: key);
+
+  final config = {
+    "Fácil": {
+      "color": AppColors.levelButtonFacil,
+      "borderColor": AppColors.levelButtonBorderFacil,
+      "fontColor": AppColors.levelButtonTextFacil,
+    },
+    "Médio": {
+      "color": AppColors.levelButtonMedio,
+      "borderColor": AppColors.levelButtonBorderMedio,
+      "fontColor": AppColors.levelButtonTextMedio,
+    },
+    "Difícil": {
+      "color": AppColors.levelButtonDificil,
+      "borderColor": AppColors.levelButtonBorderDificil,
+      "fontColor": AppColors.levelButtonTextDificil,
+    },
+    "Perito": {
+      "color": AppColors.levelButtonPerito,
+      "borderColor": AppColors.levelButtonBorderPerito,
+      "fontColor": AppColors.levelButtonTextPerito,
+    }
+  };
+
+  Color get color => config[label]!['color']!;
+  Color get borderColor => config[label]!['borderColor']!;
+  Color get fontColor => config[label]!['fontColor']!;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: AppColors.levelButtonFacil,
+        color: color,
         border: Border.fromBorderSide(
-          BorderSide(color: AppColors.levelButtonBorderFacil),
+          BorderSide(color: borderColor),
         ),
         borderRadius: BorderRadius.circular(28),
       ),
@@ -24,7 +52,7 @@ class LevelButtonWidget extends StatelessWidget {
         child: Text(
           label,
           style: GoogleFonts.notoSans(
-            color: AppColors.levelButtonTextFacil,
+            color: fontColor,
             fontSize: 13,
           ),
         ),
